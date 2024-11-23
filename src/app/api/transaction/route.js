7f9671fa435667faf8d6e36c1753c727fa1a5c37
Cristian <cristianmaya.92@hotmail.com>
2024-11-23 12:58:01 -0500

    Organizar descipcion y fecha

--- src/app/api/transaction/route.js
+++ src/app/api/transaction/route.js
@@ -1,20 +1,30 @@
 import { NextResponse } from "next/server";
 import db from "@/data/db.json";
+import { extractData } from "../genia";
 
 export async function GET() {
   return NextResponse.json(db);
 }
 
 export async function POST (request){
-  return new NextResponse(null, {
-    status: 500,
-    statusText: "Not implemented",
-  });
+  const formData = await request.formData();
+  console.log("formData", formData)
+  const description = formData.get("description");
+  const picture = formData.get("picture");
+  
+  return createTransaction(description, picture);
+
 }
 
 async function createTransaction(description, picture){
   try {
+    const result = await extractData(description, picture);
+    if (!result.date) {
+      result.date = new Date().toISOString();
+    }
     
+    console.log("ResultCreateTransaction", result)
+    return NextResponse.json({ transaction : result});
   } catch (error) {
     return new NextResponse(null, {
       status: 500,
